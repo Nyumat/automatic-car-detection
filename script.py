a5653b85332948eae97ac2a3d111ce570ef3b3d2
Thomas <nyumat18@gmail.com>
2021-03-25 22:01:55 -0700

    FINAL

--- script.py
+++ script.py
@@ -9,3 +9,22 @@ def render_cars(frame):
       for (x,y,w,h) in cars:
             cv2.rectangle(frame, (x,y), (x+w,y+h), color=(0,255,0), thickness=2)
       return frame
+
+def simulator():
+      
+      video = cv2.VideoCapture('cars.mp4')
+      while video.isOpened():
+            car, frame = video.read()
+            await_key = cv2.waitKey(1)
+            if car:
+                  car_frame = render_cars(frame)
+                  cv2.imshow('frame',car_frame)
+            else:
+                  break
+            if await_key == ord('q'):
+                  break
+      video.release()
+      cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+      simulator()
